75283bc38ef46e096005938a66c611b63fa585ba
Tripple7souls <tripple7souls@gmail.com>
2023-02-17 23:01:46 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <studio.h>
+
+/**
+ * main - print numbers in base 16
+ * Return: 0
+*/
+int main(void)
+{
+	int name;
+	char la;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (la = 'a'; la <= 'f'; la++)
+		putchar(la)
+
+	putchar('\n');
+
+	return (0);
+}
