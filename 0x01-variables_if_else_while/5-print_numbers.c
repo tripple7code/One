8a8d87aaa460fc89663732615514288be948c89e
Tripple7souls <tripple7souls@gmail.com>
2023-02-17 22:25:39 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - numbers 0 to 0
+ * Return: 0
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
