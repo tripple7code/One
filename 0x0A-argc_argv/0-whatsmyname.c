593a241a2771a2db95a710feb5a16a588823d1ca
Tripple7souls <tripple7souls@gmail.com>
2023-03-18 22:34:22 +0000

    mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * mian: prints the name of the program
+ * followed by a new line
+ *
+ * return:0
+*/
+
+int main(void)/*this is the arguement*/
+{
+printf("0-whatsmyname.c\n");
+return (0);
+}
+
+
