e6f722f1806f40bdc8fcc5ffcc0b3f7204b95498
Tripple7souls <tripple7souls@gmail.com>
2023-03-21 23:47:40 +0000

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * init_dog - initializes a structure of type dog
+ * @d: pointer to structure
+ * @name: pointer to the name of the dog
+ * @age: age of dog
+ * @owner: pointer to owner of dog
+ *
+ * Return: void
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
