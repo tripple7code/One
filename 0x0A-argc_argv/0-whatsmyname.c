c3451e366ca195fc03d6dfcc47f212dbac6cc150
Tripple7souls <tripple7souls@gmail.com>
2023-04-17 07:21:46 +0000

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,10 @@
  * return:0
 */
 
-int main(int argc, char* argv[])/*this is the arguement*/
+int main(int argc, char **argv)
 {
-printf("%s\n", argv[0]);
-return (0);
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
 }
