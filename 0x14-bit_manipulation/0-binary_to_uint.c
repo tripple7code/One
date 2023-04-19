302eab46bfef410f8c0c0cf85e48e552c533312d
Tripple7souls <tripple7souls@gmail.com>
2023-04-19 13:08:09 +0000

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,19 +12,15 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	if (!b)
+	unsigned int result = 0;
+
+	if (b == NULL)
 		return (0);
-
-	unsigned int j = 0;
-
-	for (int i = 0; b[i]; i++)
+	while (*b != '\0')
 	{
-		if (b[i] == '1')
-			j = (j << 1) | 1;
-		else if (b[i] == '0')
-			j <<= 1;
-		else
+		if (*b != '0' && *b != '1')
 			return (0);
+		result = (result << 1) + (*b++ - '0');
 	}
-	return (j);
+	return (result);
 }
